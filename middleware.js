9d32cf47ea5655811191d25f6cd816ad19338524
Srikar <msrikar328@gmail.com>
2026-04-22 16:55:05 +0530

    Implemented authentication end to end

--- middleware.js
+++ middleware.js
@@ -1,7 +1,13 @@
 module.exports.isLoggedIn = (req,res,next)=>{
+    req.session.redirectUrl = req.originalUrl;
     if(!req.isAuthenticated()){
         req.flash("error","You must be logged in");
         return res.redirect("/login");
     }
     next();
+}
+
+module.exports.redirectUrl = (req,res,next)=>{
+    res.locals.redirectUrl = req.session.redirectUrl;
+    next()
 }
--- routes/user.js
+++ routes/user.js
@@ -4,19 +4,23 @@ const wrapAsync = require('../utils/wrapAsync.js');
 const ExpressError = require('../utils/ExpressError.js');
 const User = require("../models/user.js");
 const passport = require("passport");
-
+const {redirectUrl} = require("../middleware.js")
 route.get("/signup",(req,res)=>{
     res.render("users/signup.ejs")
 })
 
-route.post("/signup",async(req,res)=>{
+route.post("/signup",async(req,res,next)=>{
     try{
     let {username,email,password} = req.body;
     let newUser = new User({email,username});
     const registeredUser = await User.register(newUser,password);
-    console.log(registeredUser);
-    req.flash("success","Welcome to MakeMyTrip");
-    res.redirect("/listings");
+    req.login(registeredUser,(err)=>{
+        if(err){
+            return next(err)
+        }
+        req.flash("success","Welcome to MakeMyTrip");
+        res.redirect("/listings");
+    })
     }
     catch(e){
         req.flash("error",e.message);
@@ -29,9 +33,10 @@ route.get("/login",(req,res)=>{
     res.render("users/login.ejs");
 })
 
-route.post("/login",passport.authenticate('local', { failureRedirect: '/login',failureFlash :true }),async(req,res)=>{
+route.post("/login",redirectUrl,passport.authenticate('local', { failureRedirect: '/login',failureFlash :true }),async(req,res)=>{
     req.flash("success","Welcome to MakeMyTrip");
-    res.redirect("/listings");
+    const redirectUrl = res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
 })
 
 route.get("/logout",(req,res,next)=>{
